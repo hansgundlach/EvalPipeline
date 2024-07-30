bdd1f63dc86e0b3b10c004560f40b433f83f7d4d
Hans Gundlach <hans.gundlach@gmail.com>
2024-07-29 23:05:04 -0400

    add load script

--- llm_load_script.py
+++ llm_load_script.py
@@ -1,26 +1,24 @@
-#%%
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import os
 from dotenv import load_dotenv
 import logging
 from torch import cuda, backends, bfloat16
-#%%
 
-#set up device 
+
+# set up device
 def get_device():
-    return 'mps' if backends.mps.is_available() else 'cuda' if cuda.is_available() else 'cpu'
+    return (
+        "mps"
+        if backends.mps.is_available()
+        else "cuda" if cuda.is_available() else "cpu"
+    )
+
 
 def load_saved_model():
     device = get_device()
     print(f"Using device = {device}")
 
 
-
-
-
-
-
-#%%
 # Enable verbose logging
 logging.basicConfig(level=logging.DEBUG)
 
@@ -43,16 +41,17 @@ model.save_pretrained(local_dir)
 
 print(f"Model and tokenizer saved to {local_dir}")
 
-# %%
 
 # Function to generate text
 def generate_text(prompt, max_length=50):
     inputs = tokenizer(prompt, return_tensors="pt")
-    outputs = model.generate(inputs.input_ids, max_length=max_length, num_return_sequences=1)
+    outputs = model.generate(
+        inputs.input_ids, max_length=max_length, num_return_sequences=1
+    )
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
 
 # Test the model with a sample prompt
 prompt = "Once upon a time"
 generated_text = generate_text(prompt)
 print(generated_text)
-# %%
